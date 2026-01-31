4f98d96b410100d18020c4f8d531f73871473c9a
Riccardo Enrico <riccardo.enrico97@gmail.com>
2026-01-31 11:28:25 +0100

    refactor: expose smppi and kmppi modules

--- src/jax_mppi/__init__.py
+++ src/jax_mppi/__init__.py
@@ -1,3 +1,14 @@
-from .mppi import MPPIConfig, MPPIState, create, command, reset, get_rollouts
+from . import kmppi, mppi, smppi
+from .mppi import MPPIConfig, MPPIState, command, create, get_rollouts, reset
 
-__all__ = ["MPPIConfig", "MPPIState", "create", "command", "reset", "get_rollouts"]
+__all__ = [
+    "MPPIConfig",
+    "MPPIState",
+    "create",
+    "command",
+    "reset",
+    "get_rollouts",
+    "mppi",
+    "smppi",
+    "kmppi",
+]
